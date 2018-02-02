5f24eb2b720d1ffed0afd7070c2b383fbf566554
luis rodriguez <lalrop92@gmail.com>
2018-02-02 16:33:31 -0300

    ejercicio 1 hash, terminado

---
+++ taller_hash.rb
@@ -0,0 +1,20 @@
+# # Ejercicio 1
+# hash = {x: 1, y:2}
+#
+# hash[:z] = 3
+# puts hash
+#
+#
+# hash[:x] = 5
+# puts hash
+#
+# hash.delete(:y)
+# puts hash
+#
+# if hash.key?(:z) ==true
+#   hash[:z] = 'yeaah'
+# end
+# puts hash
+#
+# hash_invertido = hash.invert
+# puts hash_invertido
